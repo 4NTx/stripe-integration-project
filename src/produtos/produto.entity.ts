1bdaa29665640117db7202b2185eb7e8e76664f8
4NTx <arturrrick@gmail.com>
2024-03-31 06:04:49 -0300

    Adiciona validação com class-validator a entidade de produto

--- src/produtos/produto.entity.ts
+++ src/produtos/produto.entity.ts
@@ -1,32 +1,16 @@
 import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert } from 'typeorm';
+import { IsNotEmpty, Min } from 'class-validator';
 
 @Entity('produtos')
 export class Produto {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsNotEmpty({ message: 'O nome do produto é obrigatório.' })
   @Column({ length: 255 })
   nome: string;
 
+  @Min(0, { message: 'O preço do produto deve ser maior que zero.' })
   @Column('decimal', { precision: 10, scale: 2 })
   preco: number;
-
-  @BeforeInsert()
-  private beforeInsertActions() {
-    this.nome = this.nome.trim();
-    this.validarNome();
-    this.validarPreco();
-  }
-
-  private validarNome() {
-    if (!this.nome) {
-      throw new Error('O nome do produto é obrigatório.');
-    }
-  }
-
-  private validarPreco() {
-    if (this.preco <= 0) {
-      throw new Error('O preço do produto deve ser maior que zero.');
-    }
-  }
 }
